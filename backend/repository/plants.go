23364c79417b9c85f3159595f6fd09880b53be87
Haley <haleyhoward841@gmail.com>
2026-04-20 21:17:46 -0400

    fix: handle null nickname and variety values

--- backend/repository/plants.go
+++ backend/repository/plants.go
@@ -14,6 +14,19 @@ type PlantRepository struct {
 
 var ErrNotFound = errors.New("not found")
 
+func convertIfValidStr(ns sql.NullString) string {
+	if ns.Valid {
+		return ns.String
+	}
+
+	return ""
+}
+
+func parseTime(s string) (time.Time, error) {
+	t, err := time.Parse(time.RFC3339Nano, s)
+	return t, err
+}
+
 func (r *PlantRepository) AddPlant(p *models.Plant) (*models.Plant, error) {
 	if p.Species == "" {
 		return nil, errors.New("must specify species")
@@ -41,13 +54,14 @@ func (r *PlantRepository) AddPlant(p *models.Plant) (*models.Plant, error) {
 
 	row := r.DB.QueryRow("SELECT id, species, nickname, variety, created_at, updated_at FROM plants WHERE id = ?", id)
 
+	var nickname, variety sql.NullString
 	var createdAtStr, updatedAtStr string
 
 	err = row.Scan(
 		&p.ID,
 		&p.Species,
-		&p.Nickname,
-		&p.Variety,
+		&nickname,
+		&variety,
 		&createdAtStr,
 		&updatedAtStr,
 	)
@@ -58,15 +72,20 @@ func (r *PlantRepository) AddPlant(p *models.Plant) (*models.Plant, error) {
 		return nil, err
 	}
 
-	p.CreatedAt, err = time.Parse(time.RFC3339Nano, createdAtStr)
+	createdAt, err := parseTime(createdAtStr)
 	if err != nil {
 		return nil, err
 	}
 
-	p.UpdatedAt, err = time.Parse(time.RFC3339Nano, updatedAtStr)
+	updatedAt, err := parseTime(updatedAtStr)
 	if err != nil {
 		return nil, err
 	}
+
+	p.CreatedAt = createdAt
+	p.UpdatedAt = updatedAt
+	p.Nickname = convertIfValidStr(nickname)
+	p.Variety = convertIfValidStr(variety)
 
 	return p, nil
 }
@@ -82,18 +101,35 @@ func (r *PlantRepository) ListPlants() ([]models.Plant, error) {
 
 	for rows.Next() {
 		var p models.Plant
+		var nickname, variety sql.NullString
+		var createdAtStr, updatedAtStr string
 
 		err = rows.Scan(
 			&p.ID,
 			&p.Species,
-			&p.Nickname,
-			&p.Variety,
-			&p.CreatedAt,
-			&p.UpdatedAt,
+			&nickname,
+			&variety,
+			&createdAtStr,
+			&updatedAtStr,
 		)
 		if err != nil {
 			return nil, err
 		}
+
+		createdAt, err := parseTime(createdAtStr)
+		if err != nil {
+			return nil, err
+		}
+
+		updatedAt, err := parseTime(updatedAtStr)
+		if err != nil {
+			return nil, err
+		}
+
+		p.CreatedAt = createdAt
+		p.UpdatedAt = updatedAt
+		p.Nickname = convertIfValidStr(nickname)
+		p.Variety = convertIfValidStr(variety)
 
 		plants = append(plants, p)
 	}
